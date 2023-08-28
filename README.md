ad0d4814261bef469dc4a8029fa866bbde09e897
Samuel Midgley <midgley.sam@gmail.com>
2023-08-28 11:41:39 +0100

    First commit, the start of my electron app set up to use vite, typescript, react, and tailwind.

---
+++ README.md
@@ -0,0 +1,3 @@
+# quark
+
+An Electron application with React, TypeScript, Electron, and Vite
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,5 @@
+function App(): JSX.Element {
+  return <div className="font-bold w-16 h-16">Yay</div>
+}
+
+export default App
